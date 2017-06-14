df5993bf1fe9010efec280fe850815d9ee583538
Chris Fiege <c.fiege@pengutronix.de>
2017-06-14 16:32:03 +0200

    Version Bump
    Signed-off-by: Chris Fiege <c.fiege@pengutronix.de>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="usbsdmux",
-    version="0.1.0",
+    version="0.1.1",
     description="Tool to control an usb-sd-mux from the command line",
     packages=['usbsdmux'],
     entry_points={
