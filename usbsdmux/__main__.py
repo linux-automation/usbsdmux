04e362dbf08075e79883cade093c254dc1627f5e
Chris Fiege <c.fiege@pengutronix.de>
2017-06-14 17:09:01 +0200

    Using argparse choices instead of doing it bare
    If usbsdmux was called with an undefined mode it failed silent.
    Now using an illegal mode raises an error.
    
    Additionally now client is an alias for dut.
    
    Signed-off-by: Chris Fiege <c.fiege@pengutronix.de>

--- usbsdmux/__main__.py
+++ usbsdmux/__main__.py
@@ -2,13 +2,16 @@
 
 from .usbsdmux import UsbSdMux
 import argparse
+import sys, errno
 
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("sg", help="/dev/sg* to use")
     parser.add_argument(
-        "mode", help="mode to switch to. Can be {off, DUT, host}")
+        "mode",
+        help="mode to switch to. Can be {off, DUT, host}",
+        choices=["dut", "host", "off", "client"])
 
     args = parser.parse_args()
 
@@ -17,11 +20,12 @@ def main():
     if args.mode.lower() == "off":
         ctl.mode_disconnect()
 
-    if args.mode.lower() == "dut":
+    elif args.mode.lower() == "dut" or args.mode.lower() == "client":
         ctl.mode_DUT()
 
-    if args.mode.lower() == "host":
+    elif args.mode.lower() == "host":
         ctl.mode_host()
 
+    sys.exit()
 if __name__ == "__main__":
     main()
