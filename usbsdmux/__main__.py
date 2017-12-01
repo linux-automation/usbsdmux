449fa7c8050f9e77a2c448dd30c200a920b667ef
Chris Fiege <c.fiege@pengutronix.de>
2017-12-01 16:15:58 +0100

    Enabling usdsdmux to use the service via socket
    This change enables the usbsdmux-ctl to use the systemd-service
    to control the USB-SD-Mux from an unprivileged user.
    
    Signed-off-by: Chris Fiege <c.fiege@pengutronix.de>

--- usbsdmux/__main__.py
+++ usbsdmux/__main__.py
@@ -3,6 +3,8 @@
 from .usbsdmux import UsbSdMux
 import argparse
 import sys, errno
+import json
+import socket
 
 def main():
     parser = argparse.ArgumentParser()
@@ -13,20 +15,46 @@ def main():
         help="mode to switch to",
         choices=["dut", "host", "off", "client"],
         type=str.lower)
+    parser.add_argument(
+        "-c",
+        "--client",
+        help="Run in client mode with socket /tmp/sdmux.sock",
+        action="store_true",
+        default=False)
+    parser.add_argument(
+        "-s",
+        "--socket",
+        help="Run in client mode with given socket.",
+        default="/tmp/sdmux.sock")
 
     args = parser.parse_args()
 
-    ctl = UsbSdMux(args.sg)
+    if args.client is True or args.socket is not None:
+        # socket mode
 
-    if args.mode.lower() == "off":
-        ctl.mode_disconnect()
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_SEQPACKET)
+        sock.connect(args.socket)
+        payload = dict()
+        payload["mode"] = args.mode
+        payload["sg"] = args.sg
+        sock.send(json.dumps(payload).encode())
+        print(sock.recv(4096).decode())
+        sock.close()
 
-    elif args.mode.lower() == "dut" or args.mode.lower() == "client":
-        ctl.mode_DUT()
+    else:
+        # standalone mode
 
-    elif args.mode.lower() == "host":
-        ctl.mode_host()
+        ctl = UsbSdMux(args.sg)
 
-    sys.exit()
+        if args.mode.lower() == "off":
+            ctl.mode_disconnect()
+
+        elif args.mode.lower() == "dut" or args.mode.lower() == "client":
+            ctl.mode_DUT()
+
+        elif args.mode.lower() == "host":
+            ctl.mode_host()
+
+        sys.exit()
 if __name__ == "__main__":
     main()
