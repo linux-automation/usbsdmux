d78f00c5ab3dc4a358be241c0c087c1711cf6381
Bastian Stender <bst@pengutronix.de>
2018-06-26 15:41:11 +0200

    __main__ service: print errors to stderr instead of stdout
    Signed-off-by: Bastian Stender <bst@pengutronix.de>

--- usbsdmux/__main__.py
+++ usbsdmux/__main__.py
@@ -22,10 +22,10 @@ def client_mode(sg, mode, socket_path):
     try:
         sock.connect(socket_path)
     except FileNotFoundError:
-        print("Socket path %s does not exist. Exiting." % socket_path)
+        print("Socket path %s does not exist. Exiting." % socket_path, file=sys.stderr)
         exit(1)
     except socket.error as ex:
-        print("Failed opening socket %s : %s. Exiting." % (socket_path, ex))
+        print("Failed opening socket %s : %s. Exiting." % (socket_path, ex), file=sys.stderr)
         exit(1)
     payload = dict()
     payload["mode"] = mode
@@ -68,7 +68,7 @@ def main():
     args = parser.parse_args()
 
     if args.client is True and args.direct is True:
-        print("Can not run in direct and client mode at the same time. Exiting.")
+        print("Can not run in direct and client mode at the same time. Exiting.", file=sys.stderr)
         exit(1)
 
     if args.client is True:
