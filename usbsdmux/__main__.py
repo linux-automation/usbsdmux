1f0924de8e0381b2bce2791cd16d18b6e1f362f9
Bastian Krause <bst@pengutronix.de>
2020-11-16 11:17:17 +0100

    main/service: add get action
    Add "get" to the allowed modes and call the corresponding usbsdmux
    method for direct and client mode.
    
    Signed-off-by: Bastian Krause <bst@pengutronix.de>

--- usbsdmux/__main__.py
+++ usbsdmux/__main__.py
@@ -34,6 +34,8 @@ def direct_mode(sg, mode):
         ctl.mode_DUT()
     elif mode.lower() == "host":
         ctl.mode_host()
+    elif mode.lower() == "get":
+        print(ctl.get_mode())
 
 def client_mode(sg, mode, socket_path):
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_SEQPACKET)
@@ -65,11 +67,12 @@ def main():
     parser.add_argument(
         "mode",
         help="Action:\n"
+             "get - return selected mode\n"
              "dut - set to dut mode\n"
              "client - set to dut mode (alias for dut)\n"
              "host - set to host mode\n"
              "off - set to off mode",
-        choices=["dut", "client", "host", "off"],
+        choices=["get", "dut", "client", "host", "off"],
         type=str.lower)
     parser.add_argument(
         "-d",
