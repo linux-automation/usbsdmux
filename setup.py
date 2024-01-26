89e93df939a19bab0fa980e914ed39f33afa2785
Chris Fiege <cfi@pengutronix.de>
2024-01-26 09:54:59 +0100

    setup.py: Bump package version
    Signed-off-by: Chris Fiege <cfi@pengutronix.de>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import fastentrypoints
 
 setup(
     name="usbsdmux",
-    version="24.01",
+    version="24.01.1",
     author="Chris Fiege",
     author_email="python@pengutronix.de",
     license="LGPL-2.1-or-later",
