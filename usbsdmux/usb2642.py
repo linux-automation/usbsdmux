4cdb04f8c41482cee7cf7831b4306af6e9c798c1
Chris Fiege <cfi@pengutronix.de>
2025-06-25 12:21:46 +0200

    usbsdmux: Add SPDX Copyright Information, drop License Text
    This change rewrites the Copyright Information into SPDX without
    changing it's content.
    
    This also removes the verbose license text, so that the SPDX license
    information is the single source of truth.
    
    This is done to comply with the version 3.3 of the REUSE specification
    [0].
    
    [0]: https://reuse.software/spec-3.3
    
    Signed-off-by: Chris Fiege <cfi@pengutronix.de>

--- usbsdmux/usb2642.py
+++ usbsdmux/usb2642.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python3
 
 # SPDX-License-Identifier: LGPL-2.1-or-later
-
-# Copyright (C) 2017 Pengutronix, Chris Fiege <entwicklung@pengutronix.de>
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, see <https://www.gnu.org/licenses/>.
+# SPDX-FileCopyrightText: 2017 Pengutronix, Chris Fiege <entwicklung@pengutronix.de>
 
 import ctypes
 import fcntl
